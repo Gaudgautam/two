50ff8eedf1b1343b7dffcc08072831d2c5eb71e0
sdac-2 <sdac-2@sdac-2-PC>
2024-07-30 02:46:11 +0530

    my fir

--- Two/src/two/Main.java
+++ Two/src/two/Main.java
@@ -4,5 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello world 1");
+		System.out.println("heyyy");
+		System.out.println("heooo");
 	}
 }
