6ed63dcc4c4e70d8f9c995542f9617f736503103
sdac-2 <sdac-2@sdac-2-PC>
2024-07-30 03:09:08 +0530

    my ffifif

--- Two/src/two/Main.java
+++ Two/src/two/Main.java
@@ -4,7 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello world 1");
-		System.out.println("heyyy");
-		System.out.println("heooo");
+		System.out.println("heyyy111");
+		System.out.println("heheheeheheh");
+		
 	}
 }
